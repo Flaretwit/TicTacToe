6de342719780d1cf331fab5e1b640bd795e14a36
U-BSD\296697 <296697@SHS-10D8A69B0.beaverton.k12.or.us>
2016-10-07 14:28:48 -0700

    Almost done.

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -5,63 +5,77 @@
 #include <cstdlib>
 #include <string.h>
 
-#define TIE = 1;
-#define OWIN = 2;
-#define XWIN = 3;
 using namespace std;
-struct Move;
-bool checkTie(char(*gameboard)[3]);
-bool checkWin(char player, char(*gameboard)[3]);
-void printBoard(char(*gameboard)[3]);
-Move getMove(char input[3], int player);
-char[] getInput();
+
+struct Move {
+	int x;
+	int y;
+	int player;
+};
+
+
+int checkWin(Move *m, int(*gameboard)[3]);
+void printBoard(int(*gameboard)[3]);
+Move getMove(char *input, int player);
+char* getInput();
 //a better main function
 
 int main() {
-	char move[3];
-	int owins, xins, turncount;
+
+	char* move;
+	int owins = 0;
+	int xwins = 0;
+	int turncount = 0;
 	bool keepplaying = true;
 	const int BLANK = 1;
-	const int OMOVE = 2;
-	const int XMOVE = 3;
-	Move m;
+	struct Move m;
+	m.x = 0;
+	m.y = 0;
+	m.player = 0;
+
 	//while loop for multple games
 	
 	
 	do {
-	:
-	int gameboard[3][3];
-	int** gameboard = gameboard;
+	cout << "TicTacToe on C!"
+	int gameboardthing[3][3];
+	int(*gameboard)[3] = gameboardthing;
 	//sets the gameboard
 	for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 3; j++) {
-			gameboard = BLANK;
+				gameboard[i][j] = BLANK;
 		}
 	}
 	//while no one has won or while the turncount isn't 9, runs game
-	while(!checkWin(&m, &gameboard) || turncount == 9) {
+	while(!checkWin(&m, gameboard) || turncount == 9) {
 		move = getInput();
 		m = getMove(move, turncount % 2);
 		gameboard[m.x][m.y] = m.player;
-		printBoard(&gameboard);
+		printBoard(gameboard);
 		turncount++;
 	}
+		const int TIE = 1;
+		const int OWIN = 2;
+		const int XWIN = 3;
 	//another helpful idea from Vikram.
 	//if the turncount is 9, the expression becomes equal to one
 	//which is also the value of tie, so it goes to tie in that case
-	switch(checkWin(&m, &gameboard) || turncount == 9) {
+	switch(checkWin(&m, gameboard)) {	
 		case TIE:
-			cout << "Tie!";
+			if(turncount == 9) cout << "Tie!";
+			break;
 		case OWIN:
 			cout << "O wins!";
 			owins++;
+			break;
 		case XWIN:
 			cout << "X wins!";
 			xwins++;
+			break;
 	}
 		
 
-	char choice[2];
+	char *choice;
 	
 	cout << "O wins: " + owins;
 	cout << "X wins: " + xwins;
@@ -70,37 +84,35 @@ int main() {
 	choice = getInput();
 	if(!strcmp(choice, "n")) keepplaying = false;
 	}while(keepplaying);
-c++
+
 
 }
 
 
-//from Vikram Kashyap's wonderful and intelligent code
-struct Move(int x, int y, int player) {
-
-}
 
 //checks for a win by c:w
 //hecking all the columns, rows and diagonals for a player
-//returns a boolean 
-int checkWin(Move *m, &gameboard) {
-	int counter[4] = 0;
+//returns a 2 for OWIN, 3 for XWIN and 0 for indeterminate
+//the switch statement above then checks if the indeterminate result is a tie
+int checkWin(Move *m, int(*gameboard)[3]) {
+	int counter[4];
 	for(int i = 0; i < 3; i++) {
+		counter[i] = 0;
 		//checks for the vertical column
 		if(gameboard[m->x][i] == m->player) counter[0]++;
 		//checks for the horizonal row
-		if(gameboard[i][m->] == m->player) counter[1]++;
+		if(gameboard[i][m->y] == m->player) counter[1]++;
 		//checks for topleft to bottomright diagonal
 		if(gameboard[i][i] == m->player) counter[2]++;
 		//checks for bottomleft to topright diagonal
 		if(gameboard[i][2-i] == m->player) counter[3]++;
 	}
 	for(int j = 0; j < 4; j++) {
-		if(counter[j] == 3) return player;
+		if(counter[j] == 3) return m->player;
 	}
-	return TIE;
+	return 0;
 }
-
+//prints the board
 void printBoard(int(*gameboard)[3]) {
 	cout << "\t a\t b\t c\n";
 	for(int i = 0; i < 3; i++) {
@@ -112,17 +124,19 @@ void printBoard(int(*gameboard)[3]) {
 	}
 }
 //given a character array, returns a Move object.
-Move getMove(char input[3], int player) {	
+Move getMove(char *input, int player) {	
 	char output[3];
-	cin.getLine(output, sizeof(output);
+
 	while(1) {
+		cin.getline(output, sizeof(output));
 		if(output[0] < 1 || output[0] > 3 || output[1] > 99 || output[1] < 97) {
 			cout << "Invalid input";
+			cin.clear();
+			cin.ignore(1000. '\n');
 		} else {
 			break;
 		}
 	}
-	
 	Move m;
 	m.x= input[0] - 49;
 	m.y = input[1] - 97;
@@ -130,8 +144,8 @@ Move getMove(char input[3], int player) {
 	return m;
 }
 //gets input
-char[] getInput() {
-	char input[2];
+char* getInput() {
+	char *input = new char[2];
 	bool valid = false;
 
 	while(!valid) {
@@ -139,7 +153,7 @@ char[] getInput() {
 		cin >> input;
 	       if(cin.fail()) {
 			cin.clear();
-			cin.ignore();
+			cin.ignore(1000, '\n');
 			cout << "Please enter in the format of column, row (1a).";
 			valid = false;			
 	       }	       
